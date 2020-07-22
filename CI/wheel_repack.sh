70eff5e63958c7cdedb7470a2c06ab07301786b7
Mahesh Kale <kalemaheshj@gmail.com>
2020-07-22 23:36:02 +0530

    Add CI job for testing wheel on ubuntu

--- CI/wheel_repack.sh
+++ CI/wheel_repack.sh
@@ -1,1 +1,1 @@
-wheel pack /root/phylanx-0.0.1 -d /root/
+wheel pack /github/home/phylanx-0.0.1 -d /github/home/
--- CI/wheel_skeleton.sh
+++ CI/wheel_skeleton.sh
@@ -1,7 +1,7 @@
-cp /phylanx_src/build/dist/phylanx-0.0.1-cp37-cp37m-linux_x86_64.whl /root/
-wheel unpack /root/phylanx-0.0.1-cp37-cp37m-linux_x86_64.whl
-mkdir /root/phylanx-0.0.1/phylanx-libs
-mkdir /root/phylanx-0.0.1/phylanx-libs/boost
-mkdir /root/phylanx-0.0.1/phylanx-libs/extras
-cp -r /usr/local/lib/phylanx /root/phylanx-0.0.1/phylanx-libs/
-cp -r /usr/local/lib/hpx /root/phylanx-0.0.1/phylanx-libs/
+cp /phylanx_src/build/dist/phylanx-0.0.1-cp37-cp37m-linux_x86_64.whl /github/home/
+wheel unpack /github/home/phylanx-0.0.1-cp37-cp37m-linux_x86_64.whl
+mkdir /github/home/phylanx-0.0.1/phylanx-libs
+mkdir /github/home/phylanx-0.0.1/phylanx-libs/boost
+mkdir /github/home/phylanx-0.0.1/phylanx-libs/extras
+cp -r /usr/local/lib/phylanx /github/home/phylanx-0.0.1/phylanx-libs/
+cp -r /usr/local/lib/hpx /github/home/phylanx-0.0.1/phylanx-libs/
